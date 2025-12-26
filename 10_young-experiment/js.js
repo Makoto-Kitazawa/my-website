5eb6c78bd0dede1f31bd6fa4c85ea4890ce74cb1
Makoto-Kitazawa <116934300+Makoto-Kitazawa@users.noreply.github.com>
2025-12-26 03:04:07 +0000

    Reduce destructive interference opacity to 1%
    - Changed opposite-color particle opacity from 15% to 1% during contact
    - Makes destructive interference effect more pronounced

--- 10_young-experiment/js.js
+++ 10_young-experiment/js.js
@@ -245,8 +245,8 @@ class Particle {
                     other.alpha = 1.0;
                 } else {
                     // 異色：破壊的干渉（重なっている間は薄くなる）
-                    this.alpha = 0.15;
-                    other.alpha = 0.15;
+                    this.alpha = 0.01;
+                    other.alpha = 0.01;
                 }
             }
         }
