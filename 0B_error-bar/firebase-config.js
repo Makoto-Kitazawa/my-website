b23e33d89b074b1eb97b4aee35d30cfe071edbe2
Makoto-Kitazawa <116934300+Makoto-Kitazawa@users.noreply.github.com>
2026-09-08 12:43:27 +0000

    Firebaseを実装してみた（β）

--- 0B_error-bar/firebase-config.js
+++ 0B_error-bar/firebase-config.js
@@ -1,9 +1,9 @@
-/* Replace the empty values with the Firebase Web app configuration. */
 window.firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
+  apiKey: 'AIzaSyBufhH_uPhwaTSJq37vXtTAYvILPKQnXFI',
+  authDomain: 'phys-0b-error-bar.firebaseapp.com',
+  projectId: 'phys-0b-error-bar',
+  storageBucket: 'phys-0b-error-bar.firebasestorage.app',
+  messagingSenderId: '462445197045',
+  appId: '1:462445197045:web:78c6c8296fecf0905777bc',
+  measurementId: 'G-WZKHWE48H4'
 };
