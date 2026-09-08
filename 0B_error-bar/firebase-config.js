a28f01699fea2a22cb1b948b1c93496de2fe4d8f
Makoto-Kitazawa <116934300+Makoto-Kitazawa@users.noreply.github.com>
2026-09-08 12:29:51 +0000

    Firestoreの仮実装

---
+++ 0B_error-bar/firebase-config.js
@@ -0,0 +1,9 @@
+/* Replace the empty values with the Firebase Web app configuration. */
+window.firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
+};
